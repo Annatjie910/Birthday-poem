a25d00a6738bb01bdd7296917e2d72f791b532e2
Annatjie910 <annatjie.huysamen@gmail.com>
2025-06-06 18:42:45 +0200

    test

---
+++ src/index.js
@@ -0,0 +1,27 @@
+function generatePoem(event) {
+    event.eventDefalt();
+    
+
+    let poemElement = document.querySelector("#poem")
+
+
+      new Typewriter-effect-npm,i 
+      ("#poem", 
+        string , "If your hope is strong",
+        autoStart , true,
+        cursor , "",
+        delay , 1,
+      )
+
+        
+
+
+    poemElement.innerHTML = "If your hope is strong";
+
+
+
+}
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
+
+
